a485c1bf86d53cb0ffa30df104c6dfe5474f42fb
Seb943 <55142014+Seb943@users.noreply.github.com>
2024-09-22 22:19:43 +0200

    Add files via upload

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,26 +1,29 @@
-# streamlit run "C:\Users\Utilisateur\Desktop\_Betting2024\Codes\tennis\1. StreamlitApp.py"
+# streamlit run "C:\Users\Utilisateur\Desktop\_Betting2024\Codes\tennis\StreamlitApp.py"
 
 import streamlit as st
 import os
-import time
-import re
-import requests
-import lxml
-import sys
-import json
+# import time
+# import re
+# import requests
+# import lxml
+# import sys
+# import json
 import pandas as pd
-import ast
-import datetime
-import matplotlib.pyplot as plt
+# import ast
+# import datetime
+# import matplotlib.pyplot as plt
+
+#print(os.getcwd())
 
 ################### 0 - Initialization ###################
 # 0 - Load files
-# df = pd.read_excel(r"C:\Users\Utilisateur\Desktop\_Betting2024\Data\Outputs\EDA\tennis\Backtests\Top\20240828\backtest_KNN50_250_fav0.55.xlsx")
-df = pd.read_excel("./Data/backtest_KNN50_250_fav0.55.xlsx")
+df = pd.read_excel(r"C:\Users\Utilisateur\Desktop\_Betting2024\Data\Outputs\EDA\tennis\Backtests\Top\20240828\backtest_KNN50_250_fav0.55.xlsx")
+# df = pd.read_excel("./Data/backtest_KNN50_250_fav0.55.xlsx")
 df = df[["('Date', 'Date')", "('Other', 'tournament-name')", "('Player', 'p1')", "('Player', 'p2')","('Other', 'maxOdds_1')","('Other', 'maxOdds_2')", 'modelProba1', 'modelProba2', "('modelPred', '')", 'valueBetModel', 'PL_favoriteBetModel']]
 df.columns = ["Date", "Tournament", "P1", "P2","maxOdds_1","maxOdds_2", 'modelProba1', 'modelProba2', "modelPred", 'valueBetModel', 'PL_favoriteBetModel']
 df['PnL'] = df['PL_favoriteBetModel']
 df['PnL strategy'] = df['PnL'].cumsum()
+df['Date'] = pd.to_datetime(df['Date'])
 df = df.set_index('Date')
 
 # plt.plot(pd.to_datetime(df['Date']), df['PL_favoriteBetModel'].cumsum())
